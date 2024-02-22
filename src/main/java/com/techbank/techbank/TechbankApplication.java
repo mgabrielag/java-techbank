70ebed72b11d71f41e366a99c9feb865d55102b7
Gabriela <gabriela.cgarcia1997@gmail.com>
2024-02-21 21:23:40 -0300

    feat: first commit

---
+++ src/main/java/com/techbank/techbank/TechbankApplication.java
@@ -0,0 +1,13 @@
+package com.techbank.techbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techbank/techbank/TechbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbank.techbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
